7017694d0f56b9b2feb5a857ac0c162a6c858e30
CherryBlossomMona <71821087+CherryBlossomMona@users.noreply.github.com>
2022-05-11 16:21:32 +0800

    Update CoffeeMachine.java

--- JavaBackendDeveloper/machine/CoffeeMachine.java
+++ JavaBackendDeveloper/machine/CoffeeMachine.java
@@ -5,15 +5,35 @@ import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int coffeeMachineWater = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int coffeeMachineMilk = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int coffeeMachineCoffeeBeans = scanner.nextInt();
         System.out.println("Write how many cups of coffee you will need:");
         int cups = scanner.nextInt();
-        int water = cups * 200;
-        int milk = cups * 50;
-        int coffeeBeans = cups * 15; 
+        int cupWater = coffeeMachineWater / 200;
+        int cupMilk = coffeeMachineMilk / 50;
+        int cupCoffeeBeans = coffeeMachineCoffeeBeans / 15;
+        int maxCups = 0;
+        if (cupWater <= cupMilk && cupWater <= cupCoffeeBeans) {
+            maxCups = cupWater; 
+        } else if (cupMilk <= cupWater && cupMilk <= cupCoffeeBeans) {
+            maxCups = cupMilk;
+        } else {
+            maxCups = cupCoffeeBeans;
+        }
         
-        System.out.println("For " + cups + " cups of coffee you will need:");
-        System.out.println(water + " ml of water");
-        System.out.println(milk + " ml of milkr");
-        System.out.println(coffeeBeans + " g of coffee beans");
+        if (maxCups == cups) {
+            System.out.println("Yes, I can make that amount of coffee"); 
+        } else if (maxCups > cups) {
+            int moreThan = maxCups - cups;  
+            System.out.println("Yes, I can make that amount of coffee (and even "+ moreThan +" more than that)");   
+        } else if (maxCups > cups && cups == 0) {
+            System.out.println("Yes, I can make that amount of coffee (and even 1 more than that)");
+        } else{
+            System.out.println("No, I can make only "+ maxCups +" cup(s) of coffee");
+        }    
     }
 }
