2b5683b4437a79cbbb119a9f9e1ab8079991ef99
CherryBlossomMona <71821087+CherryBlossomMona@users.noreply.github.com>
2022-08-23 23:49:33 +0800

    Update Main.java

--- JavaBackendDeveloper/converter/Main.java
+++ JavaBackendDeveloper/converter/Main.java
@@ -6,12 +6,35 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter number in decimal system: ");
-        int number = scanner.nextInt();
-        System.out.println("Enter target base: ");
-        int decimal = scanner.nextInt();
-        System.out.print("Conversion result: ");
-        System.out.print(binHexOct(number,decimal));
+        boolean cycleControl = true;
+        do {
+            System.out.println("Do you want to convert /from decimal or /to decimal? (To quit type /exit)");
+            switch (scanner.next()) {
+                case "/from" -> {
+                    System.out.println("Enter number in decimal system: ");
+                    int number = scanner.nextInt();
+                    System.out.println("Enter target base: ");
+                    int decimal = scanner.nextInt();
+                    System.out.print("Conversion result: ");
+                    System.out.print(binHexOct(number, decimal));
+                    System.out.println();
+                }
+                case "/to" -> {
+                    System.out.println("Enter source number: ");
+                    String inputNumber = scanner.next();
+                    System.out.println("Enter source base:  ");
+                    int hexadecimalNumber = scanner.nextInt();
+                    System.out.print("Conversion to decimal result: ");
+                    System.out.print(binHexOct(inputNumber, hexadecimalNumber));
+                    System.out.println();
+                }
+                case "/exit" -> {
+                    cycleControl = false;
+                }
+                default -> {
+                }
+            }
+        } while(cycleControl);
     }
     
     public static String binHexOct(int number, int decimal) {
@@ -29,5 +52,9 @@ public class Main {
                 return "invalid inputs.";
             }
         }
-    }  
+    }
+
+    public static int binHexOct(String number, int decimal) {
+        return Integer.valueOf(number, decimal);
+    }
 }
